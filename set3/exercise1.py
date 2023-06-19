d8352d60c7c798cf2747f9e19de5025378af75b9
WEIJIA1002 <lwj20011002@126.com>
2023-06-19 16:51:44 +1000

    ye

--- set3/exercise1.py
+++ set3/exercise1.py
@@ -17,7 +17,12 @@ def loop_ranger(start, stop=None, step=1):
     Look up for how range() works in the python docs. You could  answer this
     with just the range function, but we'd like you to do it the long way.
     """
-    return None
+    result = []
+    current = start
+    while current < stop if stop is not None else True:
+        result.append(current)
+        current += step
+    return result
 
 
 def two_step_ranger(start, stop):
@@ -28,7 +33,7 @@ def two_step_ranger(start, stop):
 
     You can either reuse loop_ranger, or the range function that in the standard library
     """
-    return None
+    return loop_ranger(start, stop, 2)
 
 
 def stubborn_asker(low, high):
@@ -39,7 +44,13 @@ def stubborn_asker(low, high):
 
     Look up the docs for a function called "input"
     """
-    return None
+    while True:
+        number = int(input("Enter a number between {} and {}: ".format(low, high)))
+        if low <= number <= high:
+            return number
+        else:
+            print("Number is outside the bounds. Try again.")
+
 
 
 def not_number_rejector(message):
@@ -49,7 +60,15 @@ def not_number_rejector(message):
     (e.g. "cow", "six", "8!") then throw it out and ask for an actual number.
     When you do get a number, return it.
     """
-    return None
+    while True:
+        number_str = input(message)
+        try:
+            number = int(number_str)
+            return number
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+
 
 
 def super_asker(low, high):
@@ -58,7 +77,16 @@ def super_asker(low, high):
     Combine what you learnt from stubborn_asker and not_number_rejector
     to make a function that does it all!
     """
-    return None
+    while True:
+        number_str = input("Enter a number between {} and {}: ".format(low, high))
+        try:
+            number = int(number_str)
+            if low <= number <= high:
+                return number
+            else:
+                print("Number is outside the bounds. Try again.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
 
 if __name__ == "__main__":
